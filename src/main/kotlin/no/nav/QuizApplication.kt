20407c17cc517010d642c61830ac6520f6547327
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-06-22 08:18:14 +0200

    Håndter register-team spørsmål

--- src/main/kotlin/no/nav/QuizApplication.kt
+++ src/main/kotlin/no/nav/QuizApplication.kt
@@ -10,10 +10,19 @@ import no.nav.rapid.Question
  *
  * Her skal teamet bygge ut funksjonalitet for å løse oppgavene i leesah-game.
  */
-class QuizApplication(teamName: String): QuizParticipant(teamName) {
+class QuizApplication(private val teamName: String): QuizParticipant(teamName) {
 
     override fun handle(question: Question) {
-        logger.log(question)
+
+        if(question.question == "team-registration") handleRegisterTeam(question)
+    }
+
+    /**
+     * Question handlers
+     */
+
+    private fun handleRegisterTeam(question: Question) {
+        answer(question.category, question.id(), teamName)
     }
 
     override fun handle(assessment: Assessment) {
